891df4f569744faa967d32fabad2af0507dd1ed7
Fadhil Radhian <fadhil.radhian@wartek.belajar.id>
2023-02-10 09:01:18 +0700

    add password encrypt and jwt

--- configs.js
+++ configs.js
@@ -3,8 +3,10 @@ const POSTGRES_CONFIG = {
   database: "postgres",
   password: "peni1941",
 };
+const TOKEN_EXPIRATION = 3 * 60 * 60; // 3 hours
 
 module.exports = {
   PORT,
   POSTGRES_CONFIG,
+  TOKEN_EXPIRATION,
 };
--- controllers/users.js
+++ controllers/users.js
@@ -1,3 +1,6 @@
+const bcrypt = require("bcryptjs/dist/bcrypt");
+const jwt = require("jsonwebtoken");
+const { TOKEN_EXPIRATION } = require("../configs");
 const pool = require("../database");
 const { generateRandomID } = require("../utils");
 
@@ -9,19 +12,44 @@ function getAllUsers(req, response) {
     .catch((err) => console.log(err.stack));
 }
 
-const createUser = (req, response) => {
+const createUser = async (req, response) => {
   const { username, password, role = "user" } = req.body;
+  if (!username || !password)
+    return response
+      .status(400)
+      .json({ message: "Username and password are required" });
+  const encryptedPass = await bcrypt.hashSync(password, 10);
+
   const query = {
     text: "INSERT INTO users (id, username, password, role, created_at) VALUES ($1, $2, $3, $4, $5) RETURNING *",
-    values: [generateRandomID(), username, password, role, new Date()],
+    values: [generateRandomID(), username, encryptedPass, role, new Date()],
   };
   pool
     .query(query)
     .then((res) => {
-      response.json({ user: res.rows[0] });
+      const userData = res.rows[0];
+      const token = jwt.sign(
+        {
+          id: userData.id,
+          username,
+          role,
+        },
+        `${process.env.JWT_SECRET}`,
+        {
+          expiresIn: TOKEN_EXPIRATION,
+        }
+      );
+      response.cookie("jwt", token, {
+        httpOnly: true,
+        maxAge: TOKEN_EXPIRATION * 1000, // converted to milliseconds
+      });
+      response
+        .status(201)
+        .json({ message: "User created successfully!", user: userData.id });
     })
     .catch((err) => {
       console.log(err.stack);
+      response.status(400).json({ error: err.stack });
     });
 };
 
