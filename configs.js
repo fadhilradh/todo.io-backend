21ce33055340484798b7c765754ea4722e2b5788
Fadhil Radhian <fadhil.radhian@wartek.belajar.id>
2023-02-14 20:26:12 +0700

    fix cors

--- configs.js
+++ configs.js
@@ -9,7 +9,7 @@ const TOKEN_EXPIRATION = 3 * 60 * 60; // 3 hours
 const REFRESH_TOKEN_EXPIRATION = 7 * 24 * 60 * 60; // 7 days
 
 const CORS_CONFIG = {
-  origin: "http://localhost:4000",
+  origin: "*",
   credentials: true,
 };
 
