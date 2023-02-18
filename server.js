3c4295d6014574da3f22272a0250846e759eed9b
Fadhil Radhian <fadhil.radhian@wartek.belajar.id>
2023-02-19 05:29:24 +0700

    update dotent

--- server.js
+++ server.js
@@ -2,10 +2,11 @@ const express = require("express");
 const { PORT, CORS_CONFIG } = require("./configs");
 const router = require("./routes");
 const cors = require("cors");
-const { generateRandomID } = require("./utils");
 const cookieParser = require("cookie-parser");
+const dotenv = require("dotenv").config();
 
 const app = express();
+app.use(dotenv);
 app.use(express.json());
 app.use(cors(CORS_CONFIG));
 app.use(cookieParser());
