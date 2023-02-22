08d7c1aa6d7f7f3c6d68cd08bd70c5210cf4fafb
Fadhil Radhian <fadhil.radhian@wartek.belajar.id>
2023-02-22 15:29:54 +0700

    revert user change

--- configs.js
+++ configs.js
@@ -3,7 +3,7 @@ const POSTGRES_CONFIG = {
   database: process.env.POSTGRES_DB || "postgres",
   password: process.env.POSTGRES_PASSWORD || "peni1941",
   port: process.env.POSTGRES_PORT || 5432,
-  user: process.env.POSTGRES_USER || "skygazer",
+  user: process.env.POSTGRES_USER || "postgres",
 };
 const TOKEN_EXPIRATION = 3 * 60 * 60; // 3 hours
 const REFRESH_TOKEN_EXPIRATION = 7 * 24 * 60 * 60; // 7 days
